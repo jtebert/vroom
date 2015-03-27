9df01706e96a0c3c529615d505eaa3f40adb8c41
jsalvati <jsalvati99@gmail.com>
2015-03-27 13:14:00 -0400

    adding simulator to team repo

---
+++ simulator/simulator/map.py
@@ -0,0 +1,156 @@
+
+import time
+import pygame
+from pygame. locals import *
+from math import ceil, floor, atan2, degrees, radians, sqrt
+import csv
+
+class Node(object):
+    
+    def __init__(self):
+        self.isObstacle = False
+        self.isVisited = False
+        self.dirt = -1
+        self.value = 0
+
+
+class RobotMap(object):
+    center = (0,0)
+
+    def __init__(self,w=700,h=700,cellXSize=10,cellYSize=10):
+        self.scale = 1.0
+        self.cellXSize = cellXSize
+        self.cellYSize = cellYSize
+        self.width = w
+        self.height = h
+        self.boundingBox = ((w/2,h/2),(w/2,h/2))
+        self.map = [[Node() for columns in xrange(self.width/cellXSize)] for rows in xrange(self.height/cellYSize)]
+
+    def draw(self,screen, environment = None):
+        
+        s = screen.get_size()
+        sx = self.scale*float(s[0])/self.width
+        sy = self.scale*float(s[1])/self.height
+        center = (0,0)
+
+
+        halfWidthMap = ceil(self.width/(2.*self.scale))
+        halfHeightMap = ceil(self.height/(2.*self.scale))
+
+        minx,miny,maxx,maxy =  (max(center[0]-halfWidthMap,0),
+                                max(center[1]-halfHeightMap,0),
+                                min(center[0]+halfWidthMap,self.width),
+                                min(center[1]+halfHeightMap,self.height))
+
+        cellXMax = self.width/self.cellXSize
+        cellYMax = self.height/self.cellYSize
+         
+        for x in xrange(0,int(cellXMax)):
+            for y in xrange(0,int(cellYMax)):
+                cellCornerX = x*self.cellXSize
+                cellCornerY = y*self.cellYSize
+                r = pygame.Rect(cellCornerX, cellCornerY, self.cellXSize, self.cellYSize)
+
+                if environment != None:
+                    if environment.map[y][x].value == -1:
+                        pygame.draw.rect(screen,(0,0,0),r,0)
+                    if environment.map[y][x].value == 0:
+                        pygame.draw.rect(screen,(255,255,255),r,0)
+                        
+                else:
+                    if self.map[y][x].isObstacle:
+                        pygame.draw.rect(screen,(0,0,0),r,0)
+                    elif self.map[y][x].isVisited:
+                        pygame.draw.rect(screen,(255,255,255),r,0)
+
+        #pygame.draw.rect(screen,(255,0,0),pygame.Rect((sx*(bminx-minx),sy*(bminy-miny)), (sx*(bmaxx-bminx),sy*(bmaxy-bminy))),int(1.*self.scale))
+
+
+    def drawRobot (self,screen,robot):
+        
+         
+        self.map[robot.pos[1]][robot.pos[0]].isVisited = True
+        self.map[robot.pos[1]-1][robot.pos[0]].isVisited = True
+        self.map[robot.pos[1]+1][robot.pos[0]].isVisited = True
+        self.map[robot.pos[1]-2][robot.pos[0]].isVisited = True
+        self.map[robot.pos[1]+2][robot.pos[0]].isVisited = True
+        self.map[robot.pos[1]][robot.pos[0]+1].isVisited = True
+        self.map[robot.pos[1]-1][robot.pos[0]+1].isVisited = True
+        self.map[robot.pos[1]+1][robot.pos[0]+1].isVisited = True
+        self.map[robot.pos[1]-2][robot.pos[0]+1].isVisited = True
+        self.map[robot.pos[1]+2][robot.pos[0]+1].isVisited = True
+        self.map[robot.pos[1]][robot.pos[0]-1].isVisited = True
+        self.map[robot.pos[1]-1][robot.pos[0]-1].isVisited = True
+        self.map[robot.pos[1]+1][robot.pos[0]-1].isVisited = True
+        self.map[robot.pos[1]-2][robot.pos[0]-1].isVisited = True
+        self.map[robot.pos[1]+2][robot.pos[0]-1].isVisited = True
+        self.map[robot.pos[1]][robot.pos[0]-2].isVisited = True
+        self.map[robot.pos[1]-1][robot.pos[0]-2].isVisited = True
+        self.map[robot.pos[1]+1][robot.pos[0]-2].isVisited = True
+        self.map[robot.pos[1]][robot.pos[0]+2].isVisited = True
+        self.map[robot.pos[1]-1][robot.pos[0]+2].isVisited = True
+        self.map[robot.pos[1]+1][robot.pos[0]+2].isVisited = True
+
+
+        minx,miny,maxx,maxy =  (max((robot.pos[0]*self.cellXSize)-(robot.size)/2,0),
+                                max((robot.pos[1]*self.cellYSize)-(robot.size)/2,0),
+                                min((robot.pos[0]*self.cellXSize)+(robot.size)/2,self.width),
+                                min((robot.pos[1]*self.cellYSize)+(robot.size)/2,self.height))
+        
+        
+        
+        robotCenterX = (robot.pos[0]*self.cellXSize)+(self.cellXSize/2)
+        robotCenterY = (robot.pos[1]*self.cellXSize)+(self.cellYSize/2)
+
+        pygame.draw.circle(screen,(0,0,0),(robotCenterX,robotCenterY),(robot.size)/2, 1)
+
+        if robot.heading == 'North':
+            pygame.draw.line(screen,(0,0,255),(robotCenterX,robotCenterY),(robotCenterX,robotCenterY-robot.size))
+        if robot.heading == 'South':
+            pygame.draw.line(screen,(0,0,255),(robotCenterX,robotCenterY),(robotCenterX,robotCenterY+robot.size))
+        
+        if robot.heading == 'East':
+            pygame.draw.line(screen,(0,0,255),(robotCenterX,robotCenterY),(robotCenterX+robot.size,robotCenterY))
+
+        if robot.heading == 'West':
+            pygame.draw.line(screen,(0,0,255),(robotCenterX,robotCenterY),(robotCenterX-robot.size,robotCenterY))
+
+
+class Environment(object):
+    center = (0,0)
+
+
+    def __init__(self,w=700,h=700,cellXSize=10,cellYSize=10):
+        self.cellXSize = cellXSize
+        self.cellYSize = cellYSize
+        self.width = w
+        self.height = h
+        self.boundingBox = ((w/2,h/2),(w/2,h/2))
+        self.map = [[Node() for columns in xrange(self.width/cellXSize)] for rows in xrange(self.height/cellYSize)]
+
+        self.importEnviroment('./test.csv')
+
+
+
+
+    def importEnviroment(self,csvFile):
+
+        with open(csvFile,'r') as csvFile:
+            i = 0
+            j = 0
+            reader = csv.reader(csvFile)
+            for row in reader:
+                for column in row:
+                    if j > (self.width/self.cellXSize):
+                        break
+                    self.map[i][j].value = int(column)
+                    j += 1
+                j = 0
+                i += 1
+
+                if i > (self.height/self.cellYSize):
+                    break
+            return
+                
+    
+    
---
+++ simulator/simulator/robot.py
@@ -0,0 +1,224 @@
+
+from map import *
+from math import sin,cos,pi
+
+
+
+
+class RobotSimulator(object):
+
+    def __init__(self):
+
+        # what the robot knows
+        self.map = RobotMap()
+        
+        # the world
+        self.environment = Environment()
+        self.robot = Robot(self.environment)
+
+        self.showEnvironment = False
+        self.k = None
+        self.action = None
+
+    
+    def listenControls(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                return 
+
+            
+            if event.type == pygame.KEYDOWN:
+                self.k=event.key
+            if event.type == pygame.KEYUP:
+                self.k = None
+                self.action = 'None'
+            if self.k != None:
+                if self.k == pygame.K_UP:
+                    self.action = 'North'
+                if self.k == pygame.K_DOWN:
+                    self.action = 'South'
+                if self.k == pygame.K_RIGHT:
+                    self.action = 'East'
+                if self.k == pygame.K_LEFT:
+                    self.action = 'West'
+                if self.k == pygame.K_e:
+                    self.showEnvironment = (not self.showEnvironment)
+            
+
+
+
+
+    def run(self):
+        
+        pygame.init()
+        pygame.display.set_mode((700,700),pygame.RESIZABLE)
+        pygame.display.update()
+
+
+        while(True):
+            self.listenControls()
+            screen = pygame.display.get_surface()
+
+            if self.action != None:
+                #check bumper 
+                bumpReadings = self.robot.bumpSensor(self.action,self.environment)
+                if(not(self.robot.isBump(bumpReadings))):
+                    self.robot.takeAction(self.action)
+                else:
+                    for reading in bumpReadings:
+                        self.map.map[reading[1]][reading[0]].isObstacle = True
+                self.action = None
+
+            #rect = pygame.Rect(0,0,300,300)
+            #pygame.draw.rect(screen,(200,100,20),rect,1)
+
+            screen.fill((204,204,204))  
+
+            
+            if self.showEnvironment:
+                self.map.draw(screen,environment = self.environment)
+                self.map.drawRobot
+            else:
+                self.map.draw(screen)
+                self.map.drawRobot(screen,self.robot)
+            
+
+            
+            pygame.display.update()
+
+
+        
+
+class Robot(object):
+
+    def __init__(self,environment):
+
+        self.size = 50   #5x5 cells
+        self.pos = [10,10]
+        self.heading = 'North'
+
+    def bumpSensor(self, action, environment):
+        b = []
+        
+        bumpCoordinates = self.getBumpCoordinates(action)
+
+        if bumpCoordinates != None:
+            for pos in bumpCoordinates:
+                if environment.map[pos[1]][pos[0]].value == -1:
+                    b.append(pos)
+
+        return b
+
+    def isBump(self, bumpSensor):
+        bump = False
+        if len(bumpSensor):
+            bump = True
+        return bump
+
+    def getBumpCoordinates(self, action):
+        
+        coords = None
+        
+        if self.heading == action:
+            if action == 'North':
+                coords = [[self.pos[0],self.pos[1]-3], 
+                          [self.pos[0]-1,self.pos[1]-3],
+                          [self.pos[0]+1,self.pos[1]-3],
+                          [self.pos[0]-2,self.pos[1]-2],
+                          [self.pos[0]+2,self.pos[1]-2]]
+
+            if action == 'South':
+                coords = [[self.pos[0]+1,self.pos[1]+3],
+                          [self.pos[0],self.pos[1]+3], 
+                          [self.pos[0]-1,self.pos[1]+3],
+                          [self.pos[0]-2,self.pos[1]+2],
+                          [self.pos[0]+2,self.pos[1]+2]]
+
+            if action == 'East':
+                coords = [[self.pos[0]+3,self.pos[1]], 
+                          [self.pos[0]+3,self.pos[1]-1],
+                          [self.pos[0]+3,self.pos[1]+1],
+                          [self.pos[0]+2,self.pos[1]-2],
+                          [self.pos[0]+2,self.pos[1]+2]]
+
+            if action == 'West':
+                coords = [[self.pos[0]-3,self.pos[1]], 
+                          [self.pos[0]-3,self.pos[1]-1],
+                          [self.pos[0]-3,self.pos[1]+1]]
+
+        return coords
+            
+
+
+    def dirtSensor(self):
+        d = -1
+
+        if self.environment[self.pos] > 0:
+            d = self.environment[self.pos]
+        
+        return d
+
+
+    def takeAction(self,action):
+        
+        if self.heading == action:
+            #drive forwards
+            if action == 'North':
+                #north is negative
+                self.pos[1] -= 1
+            if action == 'East':
+                self.pos[0] += 1
+            if action == 'West':
+                self.pos[0] -= 1
+            if action == 'South':
+                self.pos[1] += 1
+                
+        else:
+            #turn torwards requested action
+            self.heading = self.evaluateTurn(action)
+
+        print "robot pos: ",self.pos
+        print "robot heading: ",self.heading
+
+    def evaluateTurn(self,action):
+        
+        heading = self.heading
+
+        if self.heading == 'None':
+            heading = action
+
+        if self.heading == 'North':
+            if action == 'East':
+                heading = 'East'
+            if action == 'West':
+                heading = 'West'
+
+        if self.heading == 'East':
+            if action == 'South':
+                heading = 'South'
+            if action == 'North':
+                heading = 'North'
+
+        if self.heading == 'South':
+            if action == 'West':
+                heading = 'West'
+            if action == 'East':
+                heading = 'East'
+                
+        if self.heading == 'West':
+            if action == 'North':
+                heading = 'North'
+            if action == 'South':
+                heading = 'South'
+
+        return heading
+
+
+if __name__ == "__main__":
+    
+    #TODO add option to import environment
+    #TODO add option for real vs simulated robot
+
+    r = RobotSimulator()
+    r.run()
