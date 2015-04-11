eb7057cfcd07584ce19567e185afa2deb8ef7b99
Julia Ebert <ebertjulia@gmail.com>
2015-04-11 15:13:42 -0400

    Separated search algorithms from exploration stuff.

--- explore_map.py
+++ explore_map.py
@@ -3,9 +3,6 @@
 import utils
 
 
-# TODO: Check if there are unreachable position? (eliminate from search)
-
-
 class MapEnvironmentProblem:
     """
     A search problem of visiting (exploring) all squares in an unknown environment
@@ -43,7 +40,7 @@ class MapEnvironmentProblem:
                 next_grid = state[1].deepcopy()
                 next_grid.mark_visited((next_x, next_y))
                 next_grid.set_all_valid_actions(state[0])
-                successors.append(( (next_state, next_grid), action))
+                successors.append(((next_state, next_grid), action))
         return successors
 
     def get_cost_of_actions(self, actions):
@@ -81,121 +78,3 @@ def exploration_heuristic(state, problem):
     else:
         max_cost = 0
     return max_cost
-
-
-class Node:
-    """
-    This class contains a state, cost to reach that state (from parent or general?), last action, and reference to
-    parent Node
-    """
-
-    def __init__(self, state, prev_action, parent, problem, heuristic=utils.null_heuristic):
-        self.state = state
-        self.prev_action = prev_action
-        self.parent = parent
-        self.heuristic = heuristic
-        self.cost = self.get_cost_estimate(problem)
-
-    def __str__(self):
-        return str(self.state)
-
-    def has_parent(self):
-        """
-        Check if parent node is None
-        """
-        return self.parent is not None
-
-    def get_path_reversed(self):
-        """
-        Get the path to this Node (directions) based on parent Nodes, until no more parents (Batman!)
-        Comes out in the wrong order for Pacman so needs to be reversed
-        """
-        path = []
-        if self.has_parent():
-            path.append(self.prev_action)
-            path.extend(self.parent.get_path_reversed())
-        return path
-
-    def get_path(self):
-        """
-        Reverse the result of getPathReversed to make Pacman happy
-        """
-        path = self.get_path_reversed()
-        path.reverse()
-        return path
-
-    def get_path_cost(self, problem):
-        """
-        Get the cost from the starting position to the current Node
-        """
-        path = self.get_path()
-        return problem.get_cost_of_actions(path)
-
-    def get_cost_estimate(self, problem):
-        """
-        Get the estimated cost based on cost (from start) and heuristic (estimate to goal)
-        """
-        path_cost = self.get_path_cost(problem)
-        heuristic_cost = self.heuristic(self.state, problem)
-        return path_cost + heuristic_cost
-
-
-#TODO: This is not relevant for mapping anymore. Hang onto for dirt search later
-def a_star_search(problem, heuristic):
-    """
-    Search the problem with the heuristic to find lowest cost option
-    :param problem: Problem, e.g. MapEnvironmentProblem
-    :param heuristic: e.g. exploration_heuristic
-    :return: Actions to take
-    """
-    n0 = Node(problem.start, None, None, problem, heuristic)
-    if problem.is_goal_state(n0.state):
-        return ['None']
-    frontier = utils.PriorityQueue()
-    frontier.push(n0, n0.cost)
-    explored = set()
-    while not frontier.is_empty():
-        node = frontier.pop()
-        explored.add(node.state)
-        if problem.is_goal_state(node.state):
-            return node.getPath()
-        next_states = problem.get_successors(node.state)
-        frontier_costs = []
-        frontier_states = []
-        for n in frontier.heap:
-            frontier_states.append(n[2].state)
-            frontier_costs.append(n[2].cost)
-        for next_state in next_states:
-    
-            next_node = Node(next_state[0], next_state[1], node, problem, heuristic)
-            if (next_node.state not in explored and next_node.state not in frontier_states) or \
-                    (next_node.state in frontier_states and frontier_costs[
-                        frontier_states.index(next_node.state)] > next_node.cost):
-                frontier.push(next_node, next_node.cost)
-    return ["None"]
-
-
-# Let's try this again...
-# Depth first search, with backtracking, minimizing revisiting of visited nodes
-# End when all explored or nothing else reachable (will backtrack to starting position and have nowhere else to go)
-def depth_first_search(problem):
-    """
-    Depth first search, with backtracking, minimizing revisiting of visited nodes
-    End when all explored or nothing else reachable (will backtrack to starting position and have nowhere else to go)
-    :param problem:
-    :return: Path of actions (list)
-    """
-    node = Node(problem.start, None, None, problem)
-    while not problem.is_goal_state(node.state):
-        next_states = problem.get_successors(node.state)
-        if len(next_states) == 0:
-            # Nothing new to explore from current location; backtrack
-            next_state = node.parent.state
-        else:
-            next_state = next_states[0]
-        node = Node(next_state[0], next_state[2], node, problem)
-    return node.get_path()
-
-# Run the search:
-#problem = MapEnvironmentProblem(robot_start_state)
-#a_star_search(problem, exploration_heuristic)
--- simulator/robot.py
+++ simulator/robot.py
@@ -7,6 +7,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 from explore_map import *
+from search import *
 
 import time
 
